548e6cdf897e72333dc936faf30b2d6f2daff901
oskla <oskarlarssson@gmail.com>
2022-10-04 09:23:11 +0200

    Initial Commit

---
+++ JournalSwiftUI/JournalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalSwiftUIApp.swift
+//  JournalSwiftUI
+//
+//  Created by Oskar Larsson on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct JournalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
